4a6635796f9600026a120be8257352a2989ec2e1
Bowen Denning <bowen@dotsec.com>
2016-01-05 12:16:29 +1000

    added the main.go file as well now that keys are set in envars

---
+++ cmd/pushover/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"git.int.dotsec.com/pushover"
+)
+
+func getEnvSettings() (token string, user string, device string) {
+	token = os.Getenv("PUSHOVER_TOKEN")
+	user = os.Getenv("PUSHOVER_USER")
+	device = os.Getenv("PUSHOVER_DEVICE")
+
+	if token == "" {
+		log.Fatal("Missing environment variable settings")
+	}
+
+	if user == "" {
+		log.Fatal("Missing environment variable settings")
+	}
+
+	if device == "" {
+		log.Fatal("Missing environment variable settings")
+	}
+
+	return
+}
+
+func main() {
+	// Get the required configuration information
+	token, user, device := getEnvSettings()
+	title, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new pushover message object with our settings
+	m := pushover.NewMessage(token, user, device, title)
+
+	// Read the message from stdin and send via pushover.net
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		fmt.Fprintf(m, s.Text())
+	}
+}
